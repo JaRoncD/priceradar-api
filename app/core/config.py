1dfa33a865ec55f100504e18bfa360de31154e18
Andres R <jaroncanciod@udistrital.edu.co>
2026-03-24 21:24:14 -0500

    config update and add security

--- app/core/config.py
+++ app/core/config.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
     # Seguridad JWT
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     # CoinGecko
     COINGECKO_URL: str = "https://api.coingecko.com/api/v3"
